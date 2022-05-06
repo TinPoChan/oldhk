a2e8ce619a58144599274a3688182e1fb43acc79
Tin Po CHAN <ericchantinpo@hotmail.com>
2022-05-06 17:50:20 +0800

    In development

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import { ReactComponent as Menu } from './assets/menu.svg';
 import { ReactComponent as Question } from './assets/question.svg';
 import './App.css';
 import React, { useEffect, useState } from 'react';
-import ButtonBlock from './buttonBlock';
+import Button from 'react-bootstrap/Button';
+
 
 
 function Image(element) {
@@ -31,19 +32,90 @@ function Line() {
   )
 }
 
+// function ButtonBlock(props) {
+//   // eslint-disable-next-line react/prop-types
+//   const location = props.element.location
+//   // eslint-disable-next-line react/prop-types
+//   let arr = [...props.randomLocations]
+
+//   // console.log(location);
+
+//   // arr.push(location)
+
+//   // console.log(arr);
+
+
+
+//   // arr = shuffle(arr);
+
+//   // console.log(arr);
+
+//   const handleClick = (e) => {
+//     // eslint-disable-next-line react/prop-types
+//     // props.setIsAnswered(true);
+//     e.preventDefault();
+//     if (props)
+//       if (e.target.innerText === location) {
+//         console.log('You found the location!');
+//         e.target.className = 'btn-success';
+//       } else {
+//         console.log('You did not find the location!');
+//         e.target.className = 'btn-danger';
+//       }
+//   }
+
+
+//   return (
+//     <div className='button-container'>
+//       <Button variant="secondary" onClick={handleClick}>{arr[0]}</Button>
+//       <Button variant="secondary" onClick={handleClick}>{arr[1]}</Button>
+//       <Button variant="secondary" onClick={handleClick}>{arr[2]}</Button>
+//       <Button variant="secondary" onClick={handleClick}>{arr[3]}</Button>
+//     </div>
+//   );
+// }
+
+function shuffle(array) {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  return array;
+}
+
+
+
 function App() {
   const [element, setElement] = useState('');
   const [randomLocations, setRandomLocations] = useState([]);
+  const [isAnswered, setIsAnswered] = useState(false);
+  const [isCorrect, setIsCorrect] = useState(false);
+  const [reset, setReset] = useState(false);
+
+
 
   useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('http://localhost:3001/api/elements/random');
-      const json = await res.json();
-      setElement(json);
-    }
-
     fetchData();
   }, []);
+
+  useEffect(() => {
+    fetchData();
+  }, [reset]);
+
+  const fetchData = async () => {
+    const res = await fetch('http://localhost:3001/api/elements/random');
+    const json = await res.json();
+    setElement(json);
+  }
 
   useEffect(() => {
     if (element) {
@@ -55,30 +127,85 @@ function App() {
           for (let i = 0; i < json.length; i++) {
             setRandomLocations(prevState => [...prevState, json[i]]);
           }
+          setRandomLocations(prevState => [...prevState, element.location]);
+          setRandomLocations(prevState => shuffle(prevState));
         }
       }
-      
+
       fetchRandomLocation();
       return () => { isMounted = false }
     }
 
   }, [element]);
 
-  
-  if(element && randomLocations.length > 0) {
-    // console.log(randomLocations[0]);
+
+  const handleClick = (e) => {
+    if (!isAnswered) {
+      if (e.target.innerText === element.location) {
+        e.target.className = 'btn-success';
+        setIsCorrect(true);
+      }
+      else {
+        e.target.className = 'btn-danger';
+        setIsCorrect(false);
+      }
+      setIsAnswered(true);
+    }
+  }
+
+  const restartGame = () => {
+    setIsAnswered(false);
+    setIsCorrect(false);
+    setElement(()=>'');
+    setRandomLocations(prevState => []);
+    setReset(!reset);
+    
+  }
+
+  function info(element) {
+    return (
+      <div className='info-container'>
+        <div className='info-name'>{element.name}</div>
+        <div className='info-location'>{element.location}</div>
+        {element.year ? <div className='info-year'>{element.year}</div> : null}
+        {element.author ? <div className='info-author'>{element.author}</div> : null}
+        {element.external_url ? <div className='info-url'>{element.external_url}</div> : null}
+        <button className='cancel-button' onClick={restartGame}>Cancel</button>
+      </div>
+    );
+  }
+
+
+
     return (
       <div className="App">
 
         <HeaderBlock />
         <Line />
         <Image element={element} />
-        
-        <ButtonBlock element={element} randomLocations={randomLocations} />
+
+        {/* <ButtonBlock element={element} randomLocations={randomLocations} /> */}
+
+        <div className='button-container'>
+
+          {randomLocations.map((location, index) => {
+            return (
+              <Button variant="secondary" key={index} onClick={handleClick}>{location}</Button>
+            )
+          })}
+          
+          {/* <Button variant="secondary" onClick={handleClick}>{randomLocations[0]}</Button>
+          <Button variant="secondary" onClick={handleClick}>{randomLocations[1]}</Button>
+          <Button variant="secondary" onClick={handleClick}>{randomLocations[2]}</Button>
+          <Button variant="secondary" onClick={handleClick}>{randomLocations[3]}</Button> */}
+        </div>
+
+          {isAnswered && isCorrect && <>{info(element)}</>}
+          {isAnswered && !isCorrect && <>{info(element)}</>}
 
       </div>
     );
   }
-}
+
 
 export default App;
--- src/buttonBlock.js
+++ src/buttonBlock.js
@@ -5,21 +5,53 @@ function ButtonBlock(props) {
     // eslint-disable-next-line react/prop-types
     const location  = props.element.location
     // eslint-disable-next-line react/prop-types
-    const arr = [...props.randomLocations]
+    let arr = [...props.randomLocations]
 
-    console.log(location);
+    // console.log(location);
 
     arr.push(location)
 
-    console.log(arr);
+    // console.log(arr);
+
+    function shuffle(array) {
+        var currentIndex = array.length, temporaryValue, randomIndex;
+        // While there remain elements to shuffle...
+        while (0 !== currentIndex) {
+
+            // Pick a remaining element...
+            randomIndex = Math.floor(Math.random() * currentIndex);
+            currentIndex -= 1;
+            
+            // And swap it with the current element.
+            temporaryValue = array[currentIndex];
+            array[currentIndex] = array[randomIndex];
+            array[randomIndex] = temporaryValue;
+        }
+        return array;
+    }
+
+    arr = shuffle(arr);
+
+    // console.log(arr);
+
+    const handleClick = (e) => {
+        e.preventDefault();
+        if(e.target.innerText === location) {
+            console.log('You found the location!');
+            e.target.className = 'btn-success';
+        } else {
+          console.log('You did not find the location!');
+          e.target.className = 'btn-danger';
+        }
+    }
+
 
     return (
       <div className='button-container'>
-          
-        <Button variant="secondary">{location}</Button>
-        <Button variant="secondary"></Button>
-        <Button variant="secondary">Yau Tei</Button>
-        <Button variant="secondary">Tai Kok</Button>
+        <Button variant="secondary" onClick={handleClick}>{arr[0]}</Button>
+        <Button variant="secondary" onClick={handleClick}>{arr[1]}</Button>
+        <Button variant="secondary" onClick={handleClick}>{arr[2]}</Button>
+        <Button variant="secondary" onClick={handleClick}>{arr[3]}</Button>
       </div>
     );
   }
