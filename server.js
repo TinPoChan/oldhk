3d9ddc1263bf9d7c46c11ee0a9ae2f4a786e4e98
Tin Po CHAN <ericchantinpo@hotmail.com>
2022-05-20 22:04:11 +0800

    Update server.js

--- server.js
+++ server.js
@@ -3,8 +3,8 @@ const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
+const path = require('path');
 
-const bcrypt = require('bcrypt')
 require('express-async-errors')
 
 const locationRouter = require('./controllers/locations')
@@ -12,22 +12,24 @@ const elementRouter = require('./controllers/elements')
 const userRouter = require('./controllers/users')
 const loginRouter = require('./controllers/login')
 
-
 app.use(cors())
-
 app.use(express.json())
 app.use('/api/locations', locationRouter)
 app.use('/api/elements', elementRouter)
 app.use('/api/users', userRouter)
 app.use('/api/login', loginRouter)
 
+app.use(express.static(path.join(__dirname, 'build')));
+
+app.get('/*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+});
 
 const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
     console.log(`Server listening on ${PORT}`);
 });
-
 
 mongoose.connect(process.env.DB_URL)
     .then(() => {
